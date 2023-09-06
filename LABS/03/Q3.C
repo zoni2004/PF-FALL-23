81428e120b9d8251fc92cc42eb83fd3dcc889307
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-06 15:39:59 +0500

    Update Q3.C

--- LABS/03/Q3.C
+++ LABS/03/Q3.C
@@ -6,11 +6,12 @@ int main()
              float a;
              float b;
              float hyp;
-             printf ("Enter a side \n");
-             scanf ("%f \n" &a);
+             printf ("Enter a side");
+			 fflush(0);
+             scanf ("%f" ,&a);
              printf ("Enter the other side \n");
-             scanf ("%f \n" &b);
-             hyp = sqrt (a*a + b*b)
-             printf ("The hypotenuse is: %f \n", hyp)
+             scanf ("%f" ,&b);
+             hyp = sqrt (a*a + b*b);
+             printf ("The hypotenuse is: %f", hyp);
     return 0;
 }
