289bf96a150b56eb11b983ff4a5bab5b89aa5396
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-06 18:35:24 +0500

    Update Q5.C

--- LABS/03/Q5.C
+++ LABS/03/Q5.C
@@ -16,29 +16,31 @@ int main()
              int fuelrate = 2; 
              int fuelconsumed;
              
-             
+             do {
              printf ("Enter the distance travelled north \n");
              scanf ("%d", &Ndistance);
              printf ("Press '0' if done with input and anyother number key if not");
              scanf ("%d" ,&NdistanceC);
-             TNdistance = TNdistance + Ndistance;
              TNdistance = Ndistance;
-            
+             TNdistance = TNdistance + Ndistance;
+             } while (NdistanceC != 0);
+             do {
              printf ("Enter the distance travelled west \n");
              scanf ("%d" ,&Wdistance);
              printf ("Press '0' if done with input and anyother number key if not");
              scanf ("%d" ,&WdistanceC);
-             TWdistance = TWdistance + Wdistance;
              TWdistance = Wdistance;
+             TWdistance = TWdistance + Wdistance;
+             } while (WdistanceC != 0);
              
-           
+            do {
              printf ("Enter the distance travelled south \n");
              scanf ("%d" ,&Sdistance);
              printf ("Press '0' if done with input and anyother number key if not");
              scanf ("%d" ,&SdistanceC);
-             TSdistance = TSdistance + Sdistance;
              TSdistance = Sdistance;
-             
+             TSdistance = TSdistance + Sdistance;
+            } while (SdistanceC != 0);
              Tdistance = TSdistance+TWdistance+TNdistance;
              fuelconsumed = Tdistance * fuelrate;
              printf ("The total distance travelled is: %d \n", Tdistance);
