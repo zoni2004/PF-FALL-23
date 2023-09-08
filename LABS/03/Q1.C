8b9ac8ac294c571a818804cc6f9765488f1ac2dc
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-08 09:13:32 +0500

    Update Q1.C

--- LABS/03/Q1.C
+++ LABS/03/Q1.C
@@ -1,38 +1,54 @@
-int num1;
+#include <stdio.h>
+
+int main() {
+			 int num1;
              int num2;
              char sign;
-             float answer;
+             float ans;
+             printf("Enter an arithemtic operation: \n");
+             scanf( " %c" ,&sign);
              printf ("Enter a number:\n ");
              scanf ("%d" ,&num1);
              printf ("Enter another number: \n");
              scanf ("%d" ,&num2);
-             printf ("Enter an arithemtic operation: \n");
-             scanf ("%c" ,&sign);
+            
+			if (sign == '+')
+			  {
+			  	ans = num1 + num2;
+                 printf ("The addition of %d and %d is %f", num1, num2, ans);
+             }
              
-             if (sign == '+') {
-                 answer = num1 + num2;
-                 printf ("The answer is: %f \n", answer);
+            else if (sign == '-')
+			 {
+			 	ans = num1 - num2;
+                 printf ("The answer of %d and %d is %f", ans,num1,num2);
              }
-             else if (sign == '-') {
-                 answer = num1 - num2;
-                 printf ("The answer is: %f \n", answer);
-             }
-            else if (sign == '*') {
-                answer = num1 * num2;
-                printf ("The answer is: %f \n", answer);
+             
+            else if (sign == '*')
+			 {
+			 	ans = num1 * num2;
+                printf ("The answer of %d and &d is %f", ans,num1,num2);
             }
-            else if (sign == '/'){
+            
+            else if (sign == '/')
+			{
                 if (num2 == 0) {
-                printf ("denominator can't be zero");
+                printf ("denominator can't be zero \n");
                 scanf ("%d" ,&num2);
                 }
-                answer = num1 / num2;
-                printf ("The answer is: %f \n" , answer);
+                ans = num1 / num2;
+                printf ("The answer isof %d and %d is %f" , ans,num1,num2);
             }
+            
+            else {
+            	printf ("Invalid Input");
+			}
   
     return 0;
 }
 
     
 
+    
+  
     
