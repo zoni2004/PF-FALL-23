282742c873aa1f9069cace3729269495664d1982
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-12 22:53:41 +0500

    Update Q2.C

--- LABS/04/Q2.C
+++ LABS/04/Q2.C
@@ -5,8 +5,8 @@ int main () {
 int n;
 int count = 0;
 int i=0;
-printf ("Please enter the number of stars and lines you require \n")
-scanf ("%d" , &n)
+printf ("Please enter the number of stars and lines you require \n");
+scanf ("%d" , &n);
 while (count < n); {
 printf ("*");
 count = count++;
@@ -14,5 +14,6 @@ do {
 printf ("\n");
 i = i++;
 } while ( i!=n);
+}
  return 0;
 }
