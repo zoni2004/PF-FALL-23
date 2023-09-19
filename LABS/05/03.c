cf98f3f5a3cc58bc70dbc72f5aeabb3120a9efa1
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-19 09:10:35 +0500

    Create 03.c

---
+++ LABS/05/03.c
@@ -0,0 +1,23 @@
+char input;
+    char Y;
+    char y;
+    char N;
+    char n;
+    printf ("If you want to delete press Y/y and if not press N/n \n");
+    scanf ("%c" ,&input);
+    switch (input) {
+    case 'Y':
+    printf ("Deleted Successfully");
+    break;
+    case 'y':
+    printf ("Deleted Successfully");
+    break;
+    case 'N':
+    printf ("Delete Cancelled");
+    break;
+    case 'n':
+    printf ("Delete Cancelled");
+    break;
+    }
+    return 0;
+}
