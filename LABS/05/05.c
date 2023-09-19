5baef5fc5df5c81227335562d5c5278a0bce6080
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-19 19:00:19 +0500

    Create 05.c

---
+++ LABS/05/05.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int character;
+    printf ("Input a character \n");
+    scanf ("%c" ,&character);
+    if (character >= 'a' && character <= 'z' ){
+        printf ("Character is a small alphabet");
+    }
+    else if (character >= 'A' && character <= 'Z' ){
+        printf ("Character is a capital alphabet");
+    }
+    else if (character >= '0' && character <= '9' ){
+        printf ("Character is a digit");
+    }   
+    else {
+        printf ("Character is a special character");
+    }
+    
+    return 0;
+}
+
