8b36bb2dcab0a8ae75a08a9d00f261086183c0df
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-19 08:19:33 +0500

    Update 01.c

--- LABS/05/01.c
+++ LABS/05/01.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+    int rem;
+    printf ("Enter a number ");
+    scanf ("%d" ,&n);
+    rem = n % 3;
+    if (rem == 0) {
+    printf ("The number is a multiple of 3");
+    }
+    else { 
+    printf ("The number is not a multiple of 3");
+    }
+
+    return 0;
+}
