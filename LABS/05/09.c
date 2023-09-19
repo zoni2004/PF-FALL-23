26ac2faad490b8d70fc170dced34b7cadbbe88cc
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-19 21:29:46 +0500

    Update 09.c

--- LABS/05/09.c
+++ LABS/05/09.c
@@ -4,10 +4,11 @@
 int main() {
 float x1;
 float x2;
-float discriminant;
+float discriminant = 0;
 int a;
 int b;
 int c;
+int z;
 do{
 printf("Please enter the coffecient of x^2: \n");
 scanf("%d" ,&a); //Taking input for calculation value
@@ -15,14 +16,14 @@ printf("Please enter the coffecient of x: \n");
 scanf("%d" ,&b);
 printf("Please enter the constant: \n");
 scanf("%d" ,&c);
-discriminant = 4*a*c;
-discriminant = b*b - discriminant;
-discriminant = sqrt (discriminant);
-x1 = (-b + discriminant)/2*a;
-x2 = (-b - discriminant)/2*a;
+discriminant = b*b;
+discriminant = discriminant - 4*a*c;
+printf ("The discriminant is: %f \n" ,discriminant);
+z = sqrt (discriminant);
+x1 = (-b + z)/2*a;
+x2 = (-b - z)/2*a;
 printf ("First value of x is: %f \n",x1);
 printf ("Second value of x is: %f \n",x2);
-printf ("The discrimnat is: %f \n" ,discriminant);
 }while (a>0);
 
 return 0;
