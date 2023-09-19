b76f324cfa7b9342515b5f42e6c7bf310560cc18
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-19 08:51:51 +0500

    Create 02.c

---
+++ LABS/05/02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int current_t;
+    printf ("Enter the current time");
+    scanf ("%d" ,&current_t);
+    if (current_t >= 5 && current_t <= 11) {
+        printf ("Good Morning");
+    }
+    else if (current_t >= 12 && current_t <= 18){
+    printf ("Good Evening");
+    }
+    else if (current_t > 18 && current_t <= 24){
+    printf ("Good Night");
+    }
+    else {
+    printf ("Invalid Input");
+    }
+    return 0;
+}
