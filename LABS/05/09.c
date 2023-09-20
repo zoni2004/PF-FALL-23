eebe2f372d98f48f158fc2fb7dc4b2f4da7ab5e6
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-20 18:50:52 +0500

    Update 09.c

--- LABS/05/09.c
+++ LABS/05/09.c
@@ -4,23 +4,26 @@
 int main() {
     float x1;
     float x2;
-    float discriminant;
     int a;
     int b;
     int c;
-    discriminant = 4*a*c;
-    discriminant = b*b - discriminant;
+    int discriminant;
     printf("Please enter the coffecient of x^2: \n");
     scanf("%d" ,&a); //Taking input for calculation value
     printf("Please enter the coffecient of x: \n");
     scanf("%d" ,&b);
     printf("Please enter the constant: \n");
     scanf("%d" ,&c);
+    discriminant = 4*a;
+    discriminant = discriminant*c;
+    discriminant = (b*b)- discriminant;
     if (discriminant > 0) {
         discriminant = sqrt(discriminant);
         printf ("Roots are real and distinct \n");
-        x1 = (-b + discriminant)/2*a;
-        x2 = (-b - discriminant)/2*a;
+        x1 = (-b + discriminant);
+        x1 = x1/2*a;
+        x2 = (-b - discriminant);
+        x2 = x2/2*a;
         printf ("First value of x is: %f \n",x1);
         printf ("Second value of x is: %f \n",x2);
     }
@@ -36,7 +39,5 @@ int main() {
     else {
         printf ("Invalid Input");
     }
-
-
     return 0;
 }
