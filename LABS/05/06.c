afd56d5a143d00ab69880d93eb5b5d0f65b33969
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-20 19:12:57 +0500

    Update 06.c

--- LABS/05/06.c
+++ LABS/05/06.c
@@ -2,28 +2,28 @@
 
 int main() {
     int x;
-    int rem1;
-    int rem2;
+    int bit7;
+    int bit4;
     printf ("Input a 8-bit number \n");
     scanf ("%c" ,&x);
-    rem1 = x % ;
-    rem2 = x % ;
-    if (rem1 != 0 && rem2 != 0) {
-        x = x  ;
-        printf ("The 4th and 7th bit are now turned off %d",x);
-    }
-    else if (rem1 != 0) {
-        x = x  ;
+    bit4 = x / 1000;
+    bit4 = bit4 % 10;
+    bit7 = x / 1000000;
+    bit7 = bit7 % 10;
+    fflush (0);
+    if (bit4 == 0 && bit7 == 1) {
+        x = x - 1000000 ;
         printf ("The 7th bit is now turned off %d" ,x);
     }
-    else if (rem2 != 0) {
-        x = x  ;
+    else if (bit4 ==1 && bit7 == 0) {
+        x = x - 1000 ;
         printf ("The 4th bit is now turned off %d" ,x);
     }
-    else {
-        printf ("Both 4th and 7th bit are off already");
+    else if (bit4 == 1 && bit7 == 1) {
+        x = x - 1000000 - 1000;
+        printf ("The 4th and 7th bit are now turned off %d",x);
     }
-    
+
+
     return 0;
 }
-
