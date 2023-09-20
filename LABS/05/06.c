85a7ac3092ef82f8d2fdcb1164d5b538d71c226e
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-09-20 19:17:29 +0500

    Update 06.c

--- LABS/05/06.c
+++ LABS/05/06.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int x;
+int x;
     int bit7;
     int bit4;
     printf ("Input a 8-bit number \n");
-    scanf ("%d" ,&x);
+    scanf (" %d" ,&x);
     bit4 = x / 1000;
     bit4 = bit4 % 10;
     bit7 = x / 1000000;
@@ -13,15 +13,15 @@ int main() {
     fflush (0);
     if (bit4 == 0 && bit7 == 1) {
         x = x - 1000000 ;
-        printf ("The 7th bit is now turned off %d" ,x);
+        printf ("The 7th bit is now turned off %d");
     }
     else if (bit4 ==1 && bit7 == 0) {
         x = x - 1000 ;
-        printf ("The 4th bit is now turned off %d" ,x);
+        printf ("The 4th bit is now turned off %d");
     }
     else if (bit4 == 1 && bit7 == 1) {
         x = x - 1000000 - 1000;
-        printf ("The 4th and 7th bit are now turned off %d",x);
+        printf ("The 4th and 7th bit are now turned off %d");
     }
 
 
