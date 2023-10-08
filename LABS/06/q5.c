42210a3d253319dd4e98ca8a4dd71ed6fb35a5c6
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-10-08 13:56:33 +0500

    Update q5.c

--- LABS/06/q5.c
+++ LABS/06/q5.c
@@ -1,3 +1,16 @@
+/*
+* Programmer: Zunaira Amjad
+* Desc: printing the pattern:
+0 0 0 0
+  1 1  
+2 2 2 2
+  3 3
+4 4 4 4
+  5 5
+6 6 6 6
+* Date: 03-10-23
+*
+*/
 #include <stdio.h>
 
 int main () {
