f0f17868f014f135b3751cda3314bd8aaa42148b
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-10-10 09:15:39 +0500

    Create Q2.C

---
+++ LABS/07/Q2.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int N, d;
+    printf("Enter the size of the array: ");
+    scanf("%d", &N);
+
+    int arr[N];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the number of positions to rotate left by: ");
+    scanf("%d", &d);
+
+   
+    d %= N;  
+    for (int i = 0; i < N; i++) {
+        printf("%d ", arr[(i + d) % N]);
+    }
+
+    return 0;
+}
