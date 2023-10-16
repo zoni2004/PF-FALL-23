efaa85ffbeba5b86a8853321292d6b3f92776d95
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-10-16 20:04:00 +0500

    Update q4.c

--- LABS/07/q4.c
+++ LABS/07/q4.c
@@ -4,39 +4,55 @@
 * Date: 10-10-23
 *
 */
+
 #include <stdio.h>
 
-int main() {
+int main (){
+    //taking input of number of rows and columns of array 
+    int r,c;
+    printf ("Enter the number of rows of array");
+    scanf ("%d", &r);
+    printf ("Enter the number of columns of array");
+    scanf ("%d", &c);
+    //taking input of elements of array 
     int i,j;
-    int count = 0;
-    int arr[2][2];
-      
-    for (i=0; i<= 2;i++){
-    	for (j=0; j<=2; j++){
-    		printf ("Enter the element of arr[2][2]");
-    		scanf ("%d", &arr[i][j]);
-		}//end inner for
-	}//end outer for
-	
-	
-    for (i=0; i<= 2;i++){
-    	for (j=0; j<= 2;j++){
-    	 if (arr[i][j] == arr[j][i]){
-    	     count = count + 1;
-        }//end if
-         else {
-    	      count = count;
-	    }
-}//end inner for
-	}//end outer for
+    int isArraySymmetric = 0;
+    int a[r][c];
+    for (i=0; i<r; i++){
+        for (j=0; j<c; j++){
+            printf ("Enter the element of array 1 at %d row and %d coulmn",i,j);
+            scanf ("%d", &a[i][j]);
+        }//end inner loop for input of array 
+    }//end outer loop for input of array 
+    //checking if when rows and columns are reversed matrix does not change (taking transpose)
+    int b[c][r];
+     for (i=0; i<r; i++){
+         for (j=0; j<c; j++){
+         //checking elements of input array wwith transpose
+              if (a[i][j] != b[j][i]){
+                 printf ("Array is not Symmetric");   
+                 printf ("{");
+                 for (i=0; i<r; i++){
+                     for (j=0; j<c; j++){
+                         printf ("%d, ", a[i][j]);
+                     }//end inner loop for output of array 
+                 }//end outer loop for output of array 
+                 printf ("}"); 
+                break;
+              }
+              else {
+                isArraySymmetric = isArraySymmetric + 1;
+                 printf ("Array is Symmetric");   
+                 printf ("{");
+                 for (i=0; i<r; i++){
+                     for (j=0; j<c; j++){
+                         printf ("%d, ", a[i][j]);
+                     }//end inner loop for output of array 
+                 }//end outer loop for output of array 
+                 printf ("}"); 
+              }
+         }//end inner loop for check of elements 
+     }//end outer loop for check of elements
 
-    if (count = 0){
-    	printf ("Array is Symmetrical");
-	}
-	else {
-	 	printf ("Array is not Symmetrical");
-	}
-    return 0;
-}
-	
-
+   return 0;
+}//end main
