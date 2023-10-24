044ce21546f5484b614a61039930e85548a07c44
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-10-24 09:54:01 +0500

    Create q2.c

---
+++ LABS/08/q2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+int sum (int n1, int n2){
+    int	result = n1 + n2;
+return result;		
+}
+
+int sub (int n1, int n2){
+	 int result = n1 - n2;
+return result;		
+}
+
+int mul (int n1, int n2){
+	int result = n1 * n2;
+return result;		
+}
+
+int div (int n1, int n2){
+    int result;
+	if (n2 < 0){
+		printf ("Invalid Input");
+	}
+	else {
+	 result = n1 / n2;		
+	}
+return result;		
+}
+
+int main (){
+	int n1,n2,result;
+	char operation;
+	printf ("Enter first value \n");
+	scanf ("%d",&n1);
+	printf ("Enter second value \n");
+	scanf ("%d",&n2);
+	printf ("Enter the basic arithmetic operation you want to perform \n");
+	scanf (" %c",&operation);
+	switch(operation){
+		case '+':
+			printf ("The answer is %d", sum (n1,n2));
+			break;
+		case '-':
+			printf ("The answer is %d", sub (n1,n2));
+			break;
+		case '*':
+		    printf ("The answer is %d", mul (n1,n2));
+		    break;
+		case '/':
+			printf ("The answer is %d", div (n1,n2));
+			break;	
+	}
+	
+	
+	return 0;
+}
