0754a9b51093155c201b12e0f608dcdc3505a42c
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-10-31 09:27:30 +0500

    Create q3.c

---
+++ LABS/09/q3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main (){
+	
+	int n,i;
+	printf ("Enter number of arrays");
+	scanf ("%d", &n);
+	char my_string[n];
+	for (i=0; i<n; i++){
+		printf ("Enter a string");
+		scanf ("%s",&my_string);
+	}
+	printf ("Enter an int");
+	int my_int_arr[n];
+	for (i=0; i<n; i++){
+		scanf ("%d",&my_int_arr);
+	}
+	printf ("Enter a long long int");
+	long long int my_longlongint_arr[n];
+		for (i=0; i<n; i++){
+		scanf ("%lli", &my_longlongint_arr);
+	}
+	int *ptr;
+	ptr = my_string[0];
+	
+	for (i=0; i<n; i++){
+		printf ("my_string[%d] = %d\n",i,my_string [i]);
+		printf ("ptr + %d = %d\n",i,*(ptr+i));
+	}
+	
+	
+return 0;	
+}
