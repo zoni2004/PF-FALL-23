a41ccb9b244532625a5bedd64c09f862c0c6a073
zoni2004 <142867557+zoni2004@users.noreply.github.com>
2023-11-28 10:01:43 +0500

    Create q1.c

---
+++ LABS/12/q1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	FILE *fptr;
+	fptr = fopen("./test.csv", "r+");
+	
+	if (fptr == NULL){
+		printf ("The file is not opened.The program will exit now");
+		return 1;
+	}
+
+	else{
+	int rows=1, columns=0;
+	int i;
+	char chr = fgetc(fptr);
+	while (chr!=EOF){
+		if (chr == '\n'){
+			rows = rows + 1;
+			columns = 0;
+		}
+	chr = fgetc(fptr);
+	columns++;
+	}
+
+	fclose (fptr);
+	printf ("Number of rows is: %d\n",rows);
+	printf ("Number of columns is: %d",columns);
+return 0;
+}
+
+	
+return 0;
+}
